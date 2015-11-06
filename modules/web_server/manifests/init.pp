eafa9854349f0f12fd422367a2962588534d43db
root <root@puppet-master.(none)>
2015-11-06 11:01:03 -0500

    Add config for nginx

--- modules/web_server/manifests/init.pp
+++ modules/web_server/manifests/init.pp
@@ -6,4 +6,14 @@ class web_server {
       enable => true,
       ensure => running,
    }
+
+   file { "/etc/nginx/conf.d/wordpress.conf":
+      notify => Service['nginx'],
+      mode => "0644",
+      owner => "root",
+      group => "root",
+      require => Package['nginx'],
+      source => "puppet:///modules/web_server/wordpress.conf",
+   }
+
 }
